0c465f2d4fb8b6a2783b283cd9805e47a0218f2c
Andrew <kircheiandri@gmail.com>
2022-01-28 17:22:39 +0200

    Add build shared.

--- api/src/index.ts
+++ api/src/index.ts
@@ -5,26 +5,33 @@ import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import apiRouter from './routers/api.router';
 import { errors } from 'celebrate';
+import path from 'path';
+import proxy from 'express-http-proxy';
 
 const app = express();
 
-dotenv.config();
+dotenv.config({ path: path.join(__dirname, '..', '.env') });
 
 const PORT = process.env.PORT;
-
-app.use(bodyParser.json());
+const HOST = process.env.NODE_ENV == 'production' ? process.env.HOST : `http://localhost:${PORT}`;
+4;
 
 mongoose.connect(process.env.MONGODB_URI).then(() => {
   console.log(`He-he ....\nDB connected`);
 });
 
-app.use('/', express.static('../dist/test-project'));
-
 app.use('/api', apiRouter);
 
-app.all('*', notFoundErrorHandler);
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/*', proxy(HOST + '/*'));
+app.use(bodyParser.json());
+
+app.all('/api/*', notFoundErrorHandler);
 
 app.use(errors());
 app.use(internalServerErrorHandler);
 
-app.listen(PORT, () => console.log(`Running on PORT ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Running on HOST ${HOST}`);
+  console.log(`Running on PORT ${PORT}`);
+});
