5169308102979e9fdf84c82736f33a1c4db1d1de
Andrew <kircheiandri@gmail.com>
2022-01-31 11:41:30 +0200

    Fixed missing request body.

--- api/src/index.ts
+++ api/src/index.ts
@@ -14,17 +14,16 @@ dotenv.config({ path: path.join(__dirname, '..', '.env') });
 
 const PORT = process.env.PORT;
 const HOST = process.env.NODE_ENV == 'production' ? process.env.HOST : `http://localhost:${PORT}`;
-4;
 
 mongoose.connect(process.env.MONGODB_URI).then(() => {
   console.log(`He-he ....\nDB connected`);
 });
 
+app.use(bodyParser.json());
 app.use('/api', apiRouter);
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/*', proxy(HOST + '/*'));
-app.use(bodyParser.json());
 
 app.all('/api/*', notFoundErrorHandler);
 
